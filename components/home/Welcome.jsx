0483c902ddcc0218f03b389e4096c726ed457a71
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2023-07-13 15:46:51 +0000

    Welcome section - Home Screen

---
+++ components/home/Welcome.jsx
@@ -0,0 +1,47 @@
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather, Ionicons } from '@expo/vector-icons'
+import styles from './welcome.style'
+import { COLORS, SIZES } from '../../constants'
+
+const Welcome = () => {
+    const navigation = useNavigation()
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.welcomeText(COLORS.black, SIZES.xSmall)}>
+                    {''}
+                    Find The Most
+                </Text>
+                <Text style={styles.welcomeText(COLORS.primary, 0)}>
+                    {''}
+                    Luxurious Furniture
+                </Text>
+            </View>
+
+            <View style={styles.searchContainer}>
+                <TouchableOpacity>
+                    <Feather name='search' size={24} style={styles.searchIcon} />
+                </TouchableOpacity>
+
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value=''
+                        onPressIn={() => navigation.navigate('Search')}
+                        placeholder='What are you looking for'
+                    />
+                </View>
+
+                <View>
+                    <TouchableOpacity style={styles.searchBtn}>
+                        <Ionicons name='camera-outline' size={SIZES.xLarge} color={COLORS.offwhite} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default Welcome
---
+++ screens/search.style.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native'
+import { COLORS, SIZES } from '../constants'
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignContent: 'center',
+        marginHorizontal: SIZES.small,
+        marginVertical: SIZES.medium,
+        backgroundColor: COLORS.secondary,
+        borderRadius: SIZES.medium,
+        height: 50
+    },
+
+    searchIcon: {
+        marginHorizontal: 10,
+        marginTop: SIZES.small,
+        color: COLORS.gray
+    },
+
+    searchWrapper: {
+        flex: 1,
+        backgroundColor: COLORS.secondary,
+        marginRight: SIZES.small,
+        borderRadius: SIZES.small
+    },
+
+    searchInput: {
+        fontFamily: 'regular',
+        width: '100%',
+        height: '100%',
+        paddingHorizontal: SIZES.small,
+    },
+
+    searchBtn: {
+        width: 50,
+        height: '100%',
+        borderRadius: SIZES.medium,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.primary
+    }
+})
+
+export default styles
